aea2d78c036e018a9d7933daa75cf989e67aa856
65Mabel <mabelquayefit@gmail.com>
2022-03-24 14:03:50 -0700

    React component 4

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 import { useEffect, useState } from "react";
-
-import { ProfileCard } from "./ProfileCard";
+//import writers from "./writers";
+import ProfileCard from "./ProfileCard";
 
 function App() {
-  const [writers, setWriters] = useState([]);
+  const [data, setData] = useState({
+    writers: [],
+    loading: false,
+  });
+
+  const handleClick = () => {
+    setData((prevData) => ({
+      ...prevData,
+      loading: true,
+    }));
+    setTimeout(() => {
+      const getWriters = async () => {
+        const response = await fetch("./writers.json");
+        const data = await response.json();
+        setData({
+          writers: data,
+          loading: false,
+        });
+      };
+      getWriters();
+    }, 2000);
+  };
 
   useEffect(() => {
-    const getWriters = async () => {
-      const response = await fetch("./writers.json");
-      const data = await response.json();
-      setWriters(data);
-    };
-    getWriters();
+    handleClick();
   }, []);
+
+  if (data.loading) {
+    return (
+      <div>
+        <h1>Writer Profile</h1>
+        <div className="container">
+          <div className="card action">
+            <div className="infoText">Loading...</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1>Write Profiles</h1>
-      <div className="container">
-        {writers.map((writer) => (
-          <ProfileCard writer={writer} key={writer.id} />
-        ))}
-      </div>
+      {data.writers.length > 0 && (
+        <div className="container">
+          {data.writers.map((writer) => (
+            <ProfileCard writer={writer} key={writer.id} />
+          ))}
+        </div>
+      )}
+      {data.writers.length === 0 && (
+        <div className="container">
+          <div className="card action">
+            <div className="infoText">Oooops... no writer profile found.</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
