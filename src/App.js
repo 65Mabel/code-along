8d78303d81ba063ca1a85da48f1bc500afa4eb69
65Mabel <mabelquayefit@gmail.com>
2022-04-05 15:20:05 -0700

    React Component 9

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Navbar from "./components/Navbar/Navbar"
-
-
+import Navbar from "./components/Navbar/Navbar";
+import Blog from "./pages/Blog";
+import BlogList from "./pages/BlogList";
+import BlogDetail from "./pages/BlogDetail";
 
 const App = () => {
   return (
-   <BrowserRouter>
-  < Navbar/>
-   <Routes>
-     <Route path="about" element={<About/>}/>
-     <Route path="/" element={<Home/>}/>
-     <Route path="blog" element={<Blog/>}/>
-   </Routes>
-   </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="blog" element={<Blog />}>
+          <Route path="" element={<BlogList />} />
+          <Route path=":blog" element={<BlogDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
